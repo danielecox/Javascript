0fa4575789523c73348059b7806e8958197d9189
Daniele Marighetti Cox <daniele.marighetti@gmail.com>
2022-03-04 20:44:07 -0300

    function and clojure

--- aula.js
+++ aula.js
@@ -16,3 +16,100 @@ const soma = valores.reduce((ac, v) => ac + v)
     //git add .
     //git remote -v
     //git push github
+
+// Funções no JS
+
+function hello() {
+    console.log("hello")
+}
+
+hello()
+
+function hello() {
+    console.log("hello, " + nome)
+}
+
+hello("José")
+
+//Linguagem de múltiplas passagens, código avaliado de cima para baixo, construindo a estrutura de dados auxiliar, tabela de símbolos, guarda nome e escopo, tabela hash
+
+function soma(a, b) {
+    return a + b
+}
+
+const res = soma(2, 3)
+
+
+const dobro = function(n) {
+    return 2 * n
+}
+
+console.log(dobro(5))
+
+const triplo = function(n = 1) {
+    return n * 3
+}
+
+console.log(triplo())
+console.log(triplo(5))
+
+//ES6
+
+const hello = () => console.log("Hello")
+
+hello()
+
+const dobro = (n) => 2 * n
+    //return implícito
+
+const dobro = (n) => {
+    return 2 * n
+}
+
+const triplo = (n) => {
+    const r = n
+    r = r + n
+    r = r + n
+    return r
+}
+
+console.log(triplo(5))
+
+const continhaQualquer = (r) => {
+    return r + r + r + r + r + r + r + r + r + r
+}
+
+console.log(continhaQualquer(1))
+
+//Clojure
+//higher order function = função de alta ordem, recebe uma função
+function f(funcao) {
+    funcao()
+
+}
+
+
+//higher order function = função de alta ordem, recebe uma função
+function g() {
+    function outraFuncao() {
+        console.log("Fui produzida pela g")
+    }
+
+}
+
+const resultadoDaG = g()
+console.log(resultadoDaG)
+
+g()()
+
+f(function() {
+    console.log("Estou sendo passado para f")
+})
+
+f(1)
+
+let umaFuncao = function() {
+    console.log("Estou sendo referenciada por uma variável")
+}
+
+umaFuncao()
